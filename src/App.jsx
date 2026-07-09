77ef035c782ce83fa57cbecb05b7b1238412e06a
Ritik Parihar <ritikparihar2040@gmail.com>
2026-07-09 19:46:43 +0530

    fix: resolve case-sensitive EditProfile import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./features/login";
-import Editprofile from "./pages/Editprofile"
- 
- 
 import EditProfile from "./pages/EditProfile";
 import Otp from "./features/Otp";
 
 function App() {
   return (
     <Routes>
-       <Route path="/home" element={<Home />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/editprofile" element={<Editprofile />} />
-     <Route path="/" element={<Login />} />
-    <Route path="/otp" element={<Otp />} />
-    <Route path="/edit-profile" element={<EditProfile />} />
+      <Route path="/editprofile" element={<EditProfile />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/otp" element={<Otp />} />
+      <Route path="/edit-profile" element={<EditProfile />} />
     </Routes>
-    
   );
 }
 
